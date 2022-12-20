45b84a0d51ff224a9e08f6d53935a8df3060ecf1
drs317lakshami <devrajkumar317@gmail.com>
2022-12-20 19:57:28 +0530

    Added enhanced invoice

--- src/test/java/Cab_Invoice_Generators/CabInvoiceGeneratorTest.java
+++ src/test/java/Cab_Invoice_Generators/CabInvoiceGeneratorTest.java
@@ -18,13 +18,23 @@ public class CabInvoiceGeneratorTest {
         Assertions.assertEquals(65, fare);//Verifies that the expected and actual values are equal
     }
 
+    //calculate minimum fare using distance and time
     @Test
-    public void givenMultipleRides_ShouldReturn_TotalFare() {//Total fare for multiple rides
+    public void givenMinDistanceAndTime_ShouldReturn_MinFare() {
+        double distance = 1.5;
+        int time = 5;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);//Using object reference calling calculateFare method with parameters
+        Assertions.assertEquals(20, fare);//Verifies that the expected and actual values are equal
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturn_TotalInvoice() {
         Ride[] rides = {
                 new Ride(4.5, 10),
                 new Ride(5.5, 10)
         };
-        double totalFare = cabInvoiceGenerator.calculateFare(rides);////Using object reference calling calculateFare method with rides
-        Assertions.assertEquals(120.0, totalFare);//Verifies that the expected and actual values are equal
+        Invoice invoice = cabInvoiceGenerator.calculateFare(rides);//using object calling calculateFare method
+        Invoice expectedInvoice = new Invoice(2, 120.0);//invoice store in expectedInvoice
+        Assertions.assertEquals(invoice, expectedInvoice);
     }
 }
